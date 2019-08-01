aa24ea5a478fc6345ff5ef85b65a281c6c26995f
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-01 16:59:41 +0100

    Add project files.

---
+++ GetterAndSetters/Movies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterAndSetters
+{
+    class Movies
+    {
+
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movies(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set{
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "NR")
+                {
+                    rating = value;
+                } else
+                {
+                    rating = "NR";
+                }
+
+            }
+
+        }
+    }
+}
+
---
+++ GetterAndSetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movies avengers = new Movies("The Avengers", "Joss Wheadon", "PG-13");
+            Movies shrek = new Movies("Shrek", "Adam Adamson", "PG");
+
+
+
+            Console.WriteLine(avengers.Rating);
+            Console.ReadLine();
+        }
+    }
+}
